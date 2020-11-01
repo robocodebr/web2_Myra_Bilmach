70d56cabaf64aae368039b2a553f9bba52100705
robocodebr <robocodebr@gmail.com>
2020-11-01 19:11:19 +0300

    lesson8

---
+++ main.js
@@ -0,0 +1,15 @@
+/*console.log("Hello");
+let i = 0;
+while(i<10){
+    console.log(i)
+    i++;
+}
+let r = 0;
+while(r<20){
+    console.log(r)
+    r++;
+}
+*/
+for(let j =0; j<10; j++);
+{console.log(j);
+}
