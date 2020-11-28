2f270e23148f4cccbb449bbfeb2b1c5a767be6ad
robocodebr <robocodebr@gmail.com>
2020-11-28 18:48:13 +0300

    lesson

---
+++ LESSON_11/INDEX.HTML/script.js
@@ -0,0 +1,17 @@
+let arr =[];
+let sum = 0;
+let min = 10;
+let max = 0;
+for(let  i = 0; i<10; i++){
+    arr[i] = Math.floor(Math.random()*10);
+sum = sum+ arr[i]
+if(min>arr[i]){
+    min = arr[i];
+}
+    Math.max()
+}
+console.log(arr);
+console.log("sum"+sum);
+console.log("max"+min);
+console.log("sum"+max);
+console.log(sum);
